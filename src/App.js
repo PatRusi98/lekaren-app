772d483ce4fcdc64d18ce821e000534038e110ba
PatRusi98 <patrik.rusinak@gmail.com>
2021-11-27 19:24:59 +0100

    cart with functionalities

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Product from "./product";
 import {useEffect, useState} from "react";
 import ProductForm from "./productForm";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 function App() {
 
@@ -30,7 +31,7 @@ function App() {
             .finally(()=> setIsPending(false))
     }, [])
 
-    const handler = function (product) {
+    const addToCartHandler = function (product) {
         console.log(product.name + " clicked")
         const newCart = [...cart]
         newCart.push(product);
@@ -38,22 +39,58 @@ function App() {
         setCart(newCart)
     }
 
-  return (
-    <div className="App">
-        {<div>{cart.length}</div>}
-        {isPending && <div>Loading data...</div>}
-        {error && <div>{error}</div>}
+    const removeFromCartHandler = function (product) {
+        const newCart = [...cart]
+        const productIndex = cart.findIndex(item=> item.id === product.id)
+        newCart.splice(productIndex, 1)
+        setCart(newCart)
+    }
 
-        <div style={{
-            display: "flex",
-            flexWrap: "wrap",
-            margin: "5px",
-            padding: "5px"
-        }}>
-        {data.map(item => <Product key={item.id} product={item} onClickHandler={handler}/>)}
+  return (
+      <Router>
+        <div className="App">
+
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/edit-products">Edit Products</Link>
+                    </li>
+                    <li>
+                        <Link to="/cart">Cart</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Switch>
+                <Route path="/edit-products">
+                    <ProductForm onNewProduct={onNewProductHandler}/>
+                </Route>
+                <Route path="/cart">
+                    <h1>Cart</h1>
+                    {cart.map(item=> <div>{item.name} <button onClick={() => removeFromCartHandler(item)}/></div>)}
+                </Route>
+                <Route path="/">
+
+                    {<div>{cart.length}</div>}
+                    {isPending && <div>Loading data...</div>}
+                    {error && <div>{error}</div>}
+
+                    <div style={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        margin: "5px",
+                        padding: "5px"
+                    }}>
+                        {data.map(item => <Product key={item.id} product={item} onClickHandler={addToCartHandler}/>)}
+                    </div>
+                </Route>
+            </Switch>
+
         </div>
-        <ProductForm onNewProduct={onNewProductHandler}/>
-    </div>
+      </Router>
   );
 }
 
